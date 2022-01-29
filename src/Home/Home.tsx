4c1ac54ce7c1dc61b1110dbe7c0008d4c818dac2
Carlo <carlo@kreosoft.com>
2022-01-29 22:49:37 +0100

    nuova home

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -14,15 +14,15 @@ class Home extends React.Component<HomeProps> {
         return (<>
                 <div className={"home-first-viewed"}>
                     <div className={"box-container generic-padding"}>
-                        <div className={"box-1 felx-center"}>
+                        <div className={"box-1 "}>
+                            <div className={"image"}/>
+                        </div>
+                        <div className={"box-2 felx-center"}>
                             <p className={"fw100"}>
                                 Ti aiuto a portare le tue <span className={"enlight"}>competenze</span> al livello
                                 sucessivo, per vivere meglio attraverso il <br></br> <span
                                 className={"enlight"}>design.</span>
                             </p>
-                        </div>
-                        <div className={"box-2"}>
-                            <div className={"image"}/>
                         </div>
                     </div>
                 </div>
