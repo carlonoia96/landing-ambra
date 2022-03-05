ca82f0b5141a6a6810fb4efbd219a437b2f737b5
Carlo <carlo@kreosoft.com>
2022-03-05 11:46:02 +0100

    update

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -26,17 +26,105 @@ class Home extends React.Component<HomeProps> {
                     <div className={"filtro-scurente position-absolute"}/>
                 </div>
                 <Presentation/>
-                <div className={"generic-block-1"}>
+                <div className={"generic-block-4"}>
                     <div className={"text-container"}>
                         <p>
-                            Porto le tue competenze <br/>
-                            al livello sucessivo,<br/>
-                            per vivere meglio<br/>
-                            attraverso il<br/>
-                            <span className={"color-red"}>design</span>.
+                            Sono specializzata <br/>
+                            nello studio di<br/>
+                            <span className={"color-red grassetto"}>MATERIALI & TRENDS</span>
                         </p>
                     </div>
-                    <div className={"filtro-scurente position-absolute"}/>
+                </div>
+                <div className={"generic-block-5"}>
+                    <div className={"col1"}>
+                        <div className={"text-container"}>
+                            <p style={{marginBottom: "60px"}}>
+                                <span className={"color-red"}>Aiuto</span> le persone a vivere in <br/> <span
+                                className={"color-red"}>armonia</span> nella propria <span
+                                className={"color-red"}>casa</span>.
+                            </p>
+                            <p>
+                                La mia <span className={"color-red"}>mission</span> è coniugare <br/> funzionalità e
+                                confort abitativo<br/> con il gusto estetico.
+                            </p>
+                        </div>
+                    </div>
+                    <div className={"col2"}>
+                        <div className={"image"}/>
+                    </div>
+
+                </div>
+                <div className={"risorse"}>
+                    <p style={{fontSize: "38px"}}>LE MIE <span className={"grassetto"}>RISORSE</span> PER TE</p>
+                    <div className={"text-container"}>
+                        <p>
+                            TIPS DI DESIGN
+                        </p>
+                        <div className={"btn"}>
+                            <p>
+                                Scopri i corsi
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className={"blog"}>
+                    <div className={"text-container"}>
+                        <div className={"col1"}>
+                            <div className={"image"}/>
+                        </div>
+                        <div className={"col2"}>
+                            <div className={"text-container"}>
+                                <p style={{marginBottom: "60px"}}>
+                                    All'interno del mio <span className={"color-red"}>blog</span> potrai trovare davvero
+                                    l'idea giusta che fa per te e la tua casa, potrai scoprire molte più cose sul design
+                                    d'interni.
+                                </p>
+                                <p>
+                                    Non serve che perdi tante ore del tuo tempo prezioso a fare ricerche sull'idea di
+                                    arredamento giusta perché l'ho già fatto io per te. <br/>
+                                    Infatti, durante tutte le mie ore dedicate allo studio di vari progetti ho tratto io
+                                    stessa ispirazione da alcune foto, che oggi voglio condividere qua con te.
+                                </p>
+                                <p style={{marginTop: "60px"}}>
+                                    Partire già con l'idea giusta ti consentirà di ridurre tempi di ricerca e costi di
+                                    progettazione.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"btn"}>
+                        <div className={"image"}>
+                            <p style={{marginTop: "-1.3em"}}>Visita il blog</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={"consulenze"}>
+                    <p style={{fontSize: "38px"}}>LE MIE <span className={"grassetto"}>CONSULENZE</span> PER TE</p>
+
+                    <div className={"box-container"}>
+                        <div className={"text-container"}>
+                            <p>
+                                CONSULENZA D'ARREDO
+                            </p>
+                            <div className={"btn"}>
+                                <p>
+                                    Scopri
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className={"text-container"}>
+                            <p>
+                                STYLING CALL
+                            </p>
+                            <div className={"btn"}>
+                                <p>
+                                    Scopri
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
                 <div className={"generic-block-2"}>
                     <div className={"col1"}>
@@ -55,17 +143,29 @@ class Home extends React.Component<HomeProps> {
                         <div className={"image"}/>
                     </div>
                 </div>
-                <div className={"generic-block-3"}>
+                <div className={"generic-block-1"}>
+                    <div className={"text-container"}>
+                        <p>
+                            Porto le tue competenze <br/>
+                            al livello sucessivo,<br/>
+                            per vivere meglio<br/>
+                            attraverso il<br/>
+                            <span className={"color-red"}>design</span>.
+                        </p>
+                    </div>
+                    <div className={"filtro-scurente position-absolute"}/>
+                </div>
+                {/*<div className={"generic-block-3"}>
                     <p>
-                        La <span className={"color-red"}>chiave</span> del tuo <span className={"color-black"}>successo</span> <br/>
+                        La <span className={"color-red"}>chiave</span> del tuo <span
+                        className={"color-black"}>successo</span> <br/>
                         dipende da te!
                     </p>
                 </div>
                 <ThisIsMe/>
                 <Block/>
                 <Advertisement/>
-                <InteriorDesignMasterclass/>
-
+                <InteriorDesignMasterclass/>*/}
             </>
         )
     }
