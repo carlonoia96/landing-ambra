38fb18ad01bde27220cfb961046fc8072e95678f
carlnoia96 <carlokreosoft@gmail.com>
2022-03-06 23:29:13 +0100

    fine prima versione

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -54,6 +54,11 @@ class Home extends React.Component<HomeProps> {
                     </div>
 
                 </div>
+
+                {/*<div className={"wave-container"}>
+                    <img src={require("../assets/images/prova.png")} alt=""/>
+                </div>*/}
+
                 <div className={"risorse"}>
                     <p style={{fontSize: "38px"}}>LE MIE <span className={"grassetto"}>RISORSE</span> PER TE</p>
                     <div className={"text-container"}>
@@ -154,7 +159,186 @@ class Home extends React.Component<HomeProps> {
                         </p>
                     </div>
                     <div className={"filtro-scurente position-absolute"}/>
-                </div> 
+                </div>
+                <div className={"laurea"}>
+                    <div className={"container"}>
+                        <div className={"col1"}>
+                            <div className={"image"}/>
+                        </div>
+                        <div className={"col2"}>
+                            <div className={"text-container"}>
+                                <p className={"title"}>
+                                    Qualche curiosità su di me..
+                                </p>
+                                <p style={{marginBottom: "30px"}}>
+                                    Sai che <span className={"grassetto"}>non sono un architetto?</span> Ebbene sì <span
+                                    className={"grassetto"}>ho scelto di non esserlo!</span>
+                                </p>
+                                <p>
+                                    Mi sono <span
+                                    className={"color-red"}>laureata in Economia e gestione delle imprese</span> in
+                                    pieno Covid-19 sviluppando la tesi sulle strategie per l'acquisizione e il
+                                    mantenimento del potere di mercato. <br/>
+                                    Ho maturato una spiccata propensione per l'<span
+                                    className={"grassetto"}>imprenditorialità</span> che tutt'ora mi aiuta e mi
+                                    accompagna nel sapere ben vendere e mostrare i miei progetti d'interni al cliente.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"text-center text-container"}>
+                        <p>
+                            Volutamente non ho scelto un corso di laurea in architettura perché la mia passione e il mio
+                            obiettivo è da sempre quello di arredare abitazioni private. Perciò, avendo già, piena
+                            consapevolezza di quello che volevo fare, ho deciso di seguire degli <span
+                            className={"grassetto color-red"}>studi mirati nel settore d'interni.</span>
+                        </p>
+                        <p style={{marginTop: "30px"}}>
+                            Per il mio percorso formativo ho scelto un corso di studi professionalizzante e intensivo,
+                            che potesse fornirmi tutte le competenze necessarie per iniziare fin da subito a lavorare
+                            con i clienti.
+                        </p>
+                    </div>
+                </div>
+                <div className={"garden"}>
+                    <div className={"text-container"}>
+                        <p>
+                            Sono specializzata in <br/>
+                            <span className={"color-red grassetto"}>GARDEN DESIGN</span>
+                        </p>
+                    </div>
+                    <div className={"filtro-scurente position-absolute"}/>
+                </div>
+                <div className={"generic-block-6"}>
+                    <div className={"col1"}>
+                        <div className={"text-container"}>
+                            <p style={{marginBottom: "60px"}}>
+                                Interpreto le tue <span className={"color-red"}>esigenze</span> e <br/>
+                                i tuoi <span className={"color-red"}>desideri</span> <br/>
+                                trasformandoli in<br/>
+                                <span className={"color-red"}>realtà abitative.</span>
+                            </p>
+                            <div className={"btn"}>
+                                <p>
+                                    Scopri i corsi
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"col2"}>
+                        <div className={"image"}/>
+                    </div>
+                </div>
+
+                <div className={"lighting"}>
+                    <div className={"text-container"}>
+                        <p>
+                            Sono specializzata in <br/>
+                            <span className={"color-red grassetto"}>LIGHTING DESIGN</span>
+                        </p>
+                    </div>
+                    <div className={"filtro-scurente position-absolute"}/>
+                </div>
+                <div className={"creo standard-size-row"}>
+                    <div className={"col1"}>
+                        <div className={"image image-contain"}/>
+                    </div>
+                    <div className={"col2"}>
+                        <div className={"text-container text-center"}>
+                            <p style={{marginBottom: "60px"}}>
+                                Creo un progetto d'interni sartoriale, cucito <span
+                                className={"color-red"}>su misura</span> per te.
+                            </p>
+                            <p>
+                                La tua <span className={"color-red"}>casa</span> deve raccontare chi sei e non deve
+                                essere la copia di qualcun altro.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className={"render image-slide"}>
+                    <div className={"text-container"}>
+                        <p>
+                            Sono specializzata in <br/>
+                            <span className={"color-red grassetto"}>AUTOCAD E TECNICHE DI RENDERING</span>
+                        </p>
+                    </div>
+                    <div className={"filtro-scurente position-absolute"}/>
+                </div>
+                <div className={"diventaInterior standard-size-row"}>
+                    <div className={"col1"}>
+                        <div className={"image image-contain"}>
+                            <p style={{marginTop: "-30%"}}>
+                                Diventa Interior<br/>
+                                Designer oggi!
+                            </p>
+                        </div>
+                    </div>
+                    <div className={"col2"}>
+                        <div className={"text-container text-center"}>
+                            <p>
+                                Hai voglia di iniziare un nuovo lavoro o di cambiarlo e sei da sempre interessat* al
+                                design d'interni, ma non sai da dove iniziare o hai paura di non emergere nel settore?
+                            </p>
+                            <p style={{marginTop: "30px"}}>
+                                Nessun problema<br/>
+                                <span className={"color-red grassetto"}>Interior Design Masterclass</span> <br/>
+                                è esattamente quello che ti serve!
+                            </p>
+                            <p style={{marginTop: "30px"}}>
+                                Iscriviti oggi e avrai accesso a tutto ciò di cui hai bisogno per creare spazi
+                                funzionali, visivamente stupefacenti e stimolanti in qualsiasi capacità il tuo cuore
+                                desideri!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className={"masterClass standard-size-row"}>
+                    <div className={"col1"}>
+                        <div className={"text-container text-center"}>
+                            <p style={{marginBottom: "30px"}}>
+                                La masterclass, <br/>
+                                partendo dalle basi, formerà una figura professionale a 360°, concentrandosi tanto
+                                sull'aspetto
+                                teorico quanto su quello pratico per raggiungere una solida conoscenza.
+                            </p>
+                            <p style={{marginBottom: "60px"}}>
+                                Scopri se fa al caso tuo!
+                            </p>
+                            <div className={"btn btn-scopri"}>
+                                <p>
+                                    Scopri i corsi
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"col2"}>
+                        <div className={"image image-contain"}/>
+                    </div>
+                </div>
+                <div className={"fine standard-size-row"}>
+                    <div className={"col1"}>
+                        <div className={"image image-contain"}/>
+                    </div>
+                    <div className={"col2"}>
+                        <div className={"text-container text-center"}>
+                            <p>
+                                Entra a far parte della miglior community di Interior Design insieme a me!
+                            </p>
+                            <p style={{marginTop: "30px"}}>
+                                La tua presenza è importante per poter scambiare opinioni e idee e ricevere consigli.
+                            </p>
+                            <p style={{marginTop: "30px"}}>
+                                Vai sulla mia pagina Instagram e capirai di cosa sto parlando.
+                            </p>
+                            <div style={{marginTop: "60px"}} className={"btn btn-scopri"}>
+                                <p>
+                                    Visita
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 {/*<div className={"generic-block-3"}>
                     <p>
                         La <span className={"color-red"}>chiave</span> del tuo <span
@@ -165,7 +349,8 @@ class Home extends React.Component<HomeProps> {
                 <ThisIsMe/>
                 <Block/>
                 <Advertisement/>
-                <InteriorDesignMasterclass/>*/} 
+                <InteriorDesignMasterclass/>*/
+                }
             </>
         )
     }
