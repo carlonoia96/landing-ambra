c0c05fa0affacd4133864c8b19e3e071d0667463
carlnoia96 <carlokreosoft@gmail.com>
2022-09-05 11:22:13 +0200

    aggiunte ondine nel desktop

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -4,7 +4,9 @@ import './Home.css';
 //immagini
 import ambraHomeMobile from '../assets/images/ambra-home-mobile.webp'
 import ambraHome from '../assets/images/ambra-home.webp'
+// import pareteVerde from '../assets/images/risorse.webp'
 import pareteVerde from '../assets/images/risorse.webp'
+import waves from '../assets/images/waves.svg'
 import contattamiMobile from '../assets/images/contattami-mobile.webp'
 
 interface HomeProps {
@@ -60,6 +62,16 @@ class Home extends React.Component<HomeProps> {
                 </div>
                 <div className={"block-3"}>
                     <div className={"img"}/>
+                    <div className={"svg hide-on-mobile"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+                            <path className="elementor-shape-fill" opacity="0.33"
+                                  d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z"></path>
+                            <path className="elementor-shape-fill" opacity="0.66"
+                                  d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z"></path>
+                            <path className="elementor-shape-fill"
+                                  d="M766.1,28.9c-200-57.5-266,65.5-395.1,19.5C242,1.8,242,5.4,184.8,20.6C128,35.8,132.3,44.9,89.9,52.5C28.6,63.7,0,0,0,0 h1000c0,0-9.9,40.9-83.6,48.1S829.6,47,766.1,28.9z"></path>
+                        </svg>
+                    </div>
                     <div className={"container img text-align-center"}>
                         <p>
                             Oggi <span className={"red"}>insegno</span> a home lovers, architetti, interior designer o
